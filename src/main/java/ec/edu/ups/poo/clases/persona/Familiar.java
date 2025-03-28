1e3dfdebf4ad2382915f11358c98d3d57d1adefb
Your Name <you@example.com>
2025-03-28 12:28:26 -0500

    Initial Commit

---
+++ src/main/java/ec/edu/ups/poo/clases/persona/Familiar.java
@@ -0,0 +1,58 @@
+package ec.edu.ups.poo.clases.principal;
+
+import ec.edu.ups.poo.clases.Persona;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Familiar extends Persona {
+    private String parentesco;
+    private String tipoDeSangre;
+    private GregorianCalendar fechaNacimiento;
+    public Familiar() {}
+
+    public Familiar(String cedula, String nombre, String apellido, String direccion, GregorianCalendar fechaNacimiento, String parentesco, String tipoDeSangre) {
+        super(cedula, nombre, apellido, direccion);
+        this.parentesco = parentesco;
+        this.tipoDeSangre = tipoDeSangre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getParentesco() {
+        return parentesco;
+    }
+
+    public void setParentesco(String parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    public String getTipoDeSangre() {
+        return tipoDeSangre;
+    }
+
+    public void setTipoDeSangre(String tipoDeSangre) {
+        this.tipoDeSangre = tipoDeSangre;
+    }
+
+    public GregorianCalendar getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(GregorianCalendar fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public double calcularEdad() {
+        Calendar hoy = Calendar.getInstance();
+        int anioActual = hoy.get(Calendar.YEAR);
+        int anioNacimiento = fechaNacimiento.get(Calendar.YEAR);
+        return (anioActual - anioNacimiento);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nParentesco: " + parentesco +
+                "\nTipo de sangre: " + tipoDeSangre +
+                "\nEdad: " + calcularEdad();
+    }
+}
---
+++ src/main/java/ec/edu/ups/poo/clases/principal/ClasePrincipal.java
@@ -0,0 +1,70 @@
+package ec.edu.ups.poo.principal;
+
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+import ec.edu.ups.poo.clases.Persona;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.println("Cuantas personas quieres ingresar: ");
+        int numeroDePersonas = sc1.nextInt();
+        Persona[] personas = new Persona[numeroDePersonas];
+
+        System.out.println("Cuantos familiares quieres ingresar: ");
+        int numeroDeFamiliares = sc1.nextInt();
+        Familiar[] familiares = new Familiar[numeroDeFamiliares];
+
+        for (int i = 0; i < numeroDePersonas; i++) {
+            System.out.println("PERSONAS");
+            System.out.print("Ingrese el numero de cedula: ");
+            String cedula = sc1.next();
+            System.out.print("Ingrese el nombre: ");
+            String nombre = sc1.next();
+            System.out.print("Ingrese el apellido: ");
+            String apellido = sc1.next();
+            sc1.nextLine();
+            System.out.print("Ingrese la direccion: ");
+            String direccion = sc1.nextLine();
+            personas[i] = new Persona(cedula, nombre, apellido, direccion);
+        }
+
+        for (int i = 0; i < numeroDeFamiliares; i++) {
+            System.out.println("FAMILIARES");
+            System.out.print("Ingrese el numero de cedula: ");
+            String cedula = sc1.next();
+            System.out.print("Ingrese el nombre: ");
+            String nombre = sc1.next();
+            System.out.print("Ingrese el apellido: ");
+            String apellido = sc1.next();
+            sc1.nextLine();
+            System.out.print("Ingrese la direccion: ");
+            String direccion = sc1.nextLine();
+            System.out.print("Ingrese el parentesco: ");
+            String parentesco = sc1.next();
+            System.out.print("Ingrese el tipo de sangre: ");
+            String tipoDeSangre = sc1.next();
+            System.out.print("Ingrese el año de su nacimiento: ");
+            int anio = sc1.nextInt();
+            System.out.print("Ingrese el mes de su nacimiento: ");
+            int mes = sc1.nextInt();
+            System.out.print("Ingrese el dia de su nacimiento: ");
+            int dia = sc1.nextInt();
+            GregorianCalendar fechaNacimiento = new GregorianCalendar(anio, mes - 1, dia);
+            familiares[i] = new Familiar(cedula, nombre, apellido, direccion, fechaNacimiento, parentesco, tipoDeSangre);
+        }
+
+        System.out.println("\nLISTA DE PERSONAS:");
+        for (Persona persona : personas) {
+            System.out.println(persona);
+        }
+
+        System.out.println("\nLISTA DE FAMILIARES:");
+        for (Familiar familiar : familiares) {
+            System.out.println(familiar);
+        }
+    }
+}
+    // getIstance devuelve el metodo de ese momento
+    // Final es la manera de definir las constantes
